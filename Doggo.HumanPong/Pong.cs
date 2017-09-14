d24557ab3a4b41c1997f06e2be3c40fd64d862be
tomjan <tomjan@HI71209>
2017-09-14 11:08:24 +0200

    Renamed Game1 to Pong

--- Doggo.HumanPong/Pong.cs
+++ Doggo.HumanPong/Pong.cs
@@ -7,12 +7,12 @@ namespace Doggo.HumanPong
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class Pong : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         
-        public Game1()
+        public Pong()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
--- Doggo.HumanPong/Program.cs
+++ Doggo.HumanPong/Program.cs
@@ -13,7 +13,7 @@ namespace Doggo.HumanPong
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new Pong())
                 game.Run();
         }
     }
